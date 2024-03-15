3182993fa4e356e6c90b594d02ad1d72d680fd2d
hoover13 <hoovermtk25@gmail.com>
2024-03-15 01:36:57 -0400

    Initial Commit

---
+++ Kyat Scanner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Kyat Scanner
+//
+//  Created by Min Thu Khine on 3/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Kyat Scanner/Kyat_ScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kyat_ScannerApp.swift
+//  Kyat Scanner
+//
+//  Created by Min Thu Khine on 3/15/24.
+//
+
+import SwiftUI
+
+@main
+struct Kyat_ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
